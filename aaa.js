a9aeea57d6f0e82c1e0b10f1cd967c7681593778
a1ex <peterxhhx@gmail.com>
2022-09-20 21:43:32 +0800

    1111111

---
+++ aaa.js
@@ -0,0 +1,9 @@
+// 1. 连接仓库
+// git remote add xxx
+
+// 2.  git fetch
+// 拿到远程的分支到origin/main
+// 建立分支连接 设置上有分支main
+// git branch --set-upstream-to=origin/main
+//? 没有共同祖先无法合并
+// git merge -allow-unrelated-histories
