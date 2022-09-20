434fde18fb7fbd356c6ebe5325f0a680f5c1063b
a1ex <peterxhhx@gmail.com>
2022-09-20 21:50:43 +0800

    2222

--- aaa.js
+++ aaa.js
@@ -3,7 +3,10 @@
 
 // 2.  git fetch
 // 拿到远程的分支到origin/main
+
 // 建立分支连接 设置上有分支main
 // git branch --set-upstream-to=origin/main
 //? 没有共同祖先无法合并
-// git merge -allow-unrelated-histories
+// git merge --allow-unrelated-histories
+// 配置push使用上有分支
+// git config push.default upstream
